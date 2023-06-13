6c9c1dbf5f8e82a49cc9485988dd92d9d1582527
cc <codecolorist@gmail.com>
2023-06-14 01:08:49 +0200

    add ios-pull command

--- tools/pull.ts
+++ tools/pull.ts
@@ -1,17 +1,32 @@
 import { Command } from 'commander';
 
-import { connect, download } from '../modules/ssh.js';
 import useCommonArgs from '../middlewares/args.js';
 import { getDeviceFromArg } from '../middlewares/device.js';
+import { Pull } from '../modules/scp.js';
+import { connect } from '../modules/ssh.js';
 
 async function main() {
-  const program = useCommonArgs(new Command('pull'));
+  const program = useCommonArgs(new Command('pull'))
+    .parse(process.argv);
 
-  const device = await getDeviceFromArg(program.parse(process.argv));
+  if (program.args.length < 2) {
+    program.help();
+  }
+
+  const device = await getDeviceFromArg(program);
   const client = await connect(device);
 
+  const sources = program.args.slice(0, -1);
+  const destination = program.args[program.args.length - 1];
+
   try {
-    await download(client, '/private/var/containers/Bundle/Application/EB6E9437-2E30-4618-A7E3-53012E663E8E/stable.app', './stable.app', true);
+    for (const source of sources) {
+      const pull = new Pull(client, source, destination);
+      pull.receiver.on('done', (src) => {
+        console.info(`${src}`);
+      });
+      await pull.execute();
+    }
   } finally {
     client.end();
   }
