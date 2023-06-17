95520e4cc2ea1d11aaf1d38b307f72c5d1dba9c1
cc <codecolorist@gmail.com>
2023-06-17 14:19:36 +0200

    add package version to cli usage

--- middlewares/args.ts
+++ middlewares/args.ts
@@ -1,5 +1,7 @@
 import { Command } from 'commander';
 
+import pkg from '../package.json' assert { type: "json" };
+
 export interface Options {
   usb: boolean;
   remote: boolean;
@@ -11,6 +13,7 @@ export type Program = Command & Options;
 
 export default function useCommonArgs(program: Command) {
   return program
+    .version(pkg.version)
     .option('-U, --usb', 'connect to USB device')
     .option('-R, --remote', 'connect to remote frida-server')
     .option('-D, --device <uuid>', 'connect to device with the given ID')
